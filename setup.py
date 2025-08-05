0a1ce8ce42f68cd7fdd167ebcb8fbe00fa920381
DataShoaib <mdshoaiba478@gmail.com>
2025-08-05 17:50:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a model for practicing pipeline and code+data versioning and experiment tracking',
+    author='md shoaib akhtar',
+    license='',
+)
